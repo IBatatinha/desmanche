b8b71fa3686193b5e9bd5c772bfe3d2f6abbd16f
IBatinha <pedropaulohiroshi@gmail.com>
2025-08-08 22:07:55 -0300

    Initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+
+shared_scripts {
+  -- '@vrp/lib/utils.lua',
+  'shared/main.lua',
+  'shared/config.lua'
+}
+
+client_scripts {
+  'client/marker.lua'
+}
+
+server_scripts {}
